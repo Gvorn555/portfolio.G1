43904a4d7e4395d3e3d815ae9dcbb02dbbe790d7
gvorn <142647609+Gvorn555@users.noreply.github.com>
2023-12-29 00:46:05 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
-import '../styles/component/header.css';
 import '../styles/component/hero.css';
 import '../styles/component/about.css';
 import '../styles/component/work.css';
 import '../styles/component/footer.css';
-import '../styles/component/mobile-nav.css';
 import '../styles/component/top.css'
 import '../styles/utils.css';
 
